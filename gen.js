db41590e4a8557eda912cb2eb0ee226353c28195
Leigh Johnson <leigh.carterjohnson@gmail.com>
2021-08-06 14:35:49 -0700

    finish gen

--- gen.js
+++ gen.js
@@ -1,11 +1,34 @@
 function gen(data) {
     return `
-    ${data.title};
-    ${data.description};
-    ${data.install};
-    ${data.usage};
-    ${data.contributing};
-    ${data.tests};
+    # ${data.title};
+
+    ## Table of Contents:
+        *[Description](#description)
+        *[Installation](#installation)
+        *[Usage](#usage)
+        *[Contributing](#contributors)
+        *[Tests](#tests)
+        *[Questions](#questions)
+        *[License](#license)
+
+    #Description
+        ${data.description};
+    #Installation
+        ${data.install};
+    #Usage
+        ${data.usage};
+    #Contributors
+        ${data.contributing};
+    #Tests
+        ${data.tests};
+    # Questions
+        Feel free to reach out to me if you have questions!
+        Github: [https://github.com/${data.github}](https://github.com/${data.github})
+        Email: [${data.email}](${data.email})
+
+    # License
+        [${data.license}](https://opensource.org/licenses)
+        CLICK TO VIEW TERMS AND CONDITIONS OF ALL OPEN SOURCE LICENSES.
     `;
 };
 
