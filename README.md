5ba665658daac6e11f2a73ea7250f9d9e55f7d71
Leigh Johnson <leigh.carterjohnson@gmail.com>
2021-08-06 15:58:08 -0700

    readme

--- README.md
+++ README.md
@@ -1,38 +1,39 @@
 
-# Read Meh;
+# Read Meh
 
 [![License](https://img.shields.io/badge/License-Unlicense%20-blue.svg)](https://opensource.org/licenses/Unlicense)
 
 ## Table of Contents:
-    * [Description](#description)
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [Contributing](#contributors)
-    * [Tests](#tests)
-    * [Questions](#questions)
-    * [License](#license)
 
-# Description
-readme generator;
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributors)
+* [Tests](#tests)
+* [Questions](#questions)
+* [License](#license)
 
-# Installation
-node index.js;
+## Description
+readme generator
 
-# Usage
-run command;
+## Installation
+node index.js
 
-# Contributors
-me;
+## Usage
+run command
 
-# Tests
-none;
+## Contributors
+me
 
-# Questions
+## Tests
+none
+
+## Questions
 Feel free to reach out to me if you have questions!
 Github: [https://github.com/uglyb0nes](https://github.com/uglyb0nes)
 Email: [leigh.carterjohnson@gmail.com](leigh.carterjohnson@gmail.com)
 
-# License
+## License
 [Unlicense](https://opensource.org/licenses)
 CLICK TO VIEW TERMS AND CONDITIONS OF ALL OPEN SOURCE LICENSES.
     
--- gen.js
+++ gen.js
@@ -1,39 +1,40 @@
 function gen(data) {
     return `
-# ${data.title};
+# ${data.title}
 
 [![License](https://img.shields.io/badge/License-${data.license}%20-blue.svg)](https://opensource.org/licenses/${data.license})
 
 ## Table of Contents:
-    * [Description](#description)
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [Contributing](#contributors)
-    * [Tests](#tests)
-    * [Questions](#questions)
-    * [License](#license)
 
-# Description
-${data.description};
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributors)
+* [Tests](#tests)
+* [Questions](#questions)
+* [License](#license)
 
-# Installation
-${data.install};
+## Description
+${data.description}
 
-# Usage
-${data.usage};
+## Installation
+${data.install}
 
-# Contributors
-${data.contributing};
+## Usage
+${data.usage}
 
-# Tests
-${data.tests};
+## Contributors
+${data.contributing}
 
-# Questions
+## Tests
+${data.tests}
+
+## Questions
 Feel free to reach out to me if you have questions!
 Github: [https://github.com/${data.github}](https://github.com/${data.github})
 Email: [${data.email}](${data.email})
 
-# License
+## License
 [${data.license}](https://opensource.org/licenses)
 CLICK TO VIEW TERMS AND CONDITIONS OF ALL OPEN SOURCE LICENSES.
     `;
